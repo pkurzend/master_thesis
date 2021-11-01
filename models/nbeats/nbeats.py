fix last commit scaling problem in nbeats without flow

models/nbeats/nbeats.py
@@ -109,15 +109,20 @@ class MultivariateNBeats(torch.nn.Module):
 
 
 
-        x =  torch.cat((x_ts, x_s, x_tf), dim=-1) # shape: (N, context_length, input_dim)
-        N, E, S = x.shape
+        # x =  torch.cat((x_ts, x_s, x_tf), dim=-1) # shape: (N, context_length, input_dim)
+        x = x_ts
+        N, S, E = x.shape
 
         # forecast = x[:, :self.output_dim, -1:]
         forecast = x[:, :self.output_dim, -1:]*0 # (N, E, 1) 
+        print('x.shape ', x.shape)
+        print('forecast.shape ', forecast.shape)
+        
 
 
         # flatten input:
         x = x.reshape(x.shape[0], -1) # (N, S*E)
+        print('x_reshaped.shape ', x.shape)
     
 
         
@@ -125,19 +130,20 @@ class MultivariateNBeats(torch.nn.Module):
         # input_mask = torch.ones(x.shape).to(device)
         input_mask = pad_mask.unsqueeze(1).expand(-1, x.shape[1], -1)# (N, E, S)
         input_mask = input_mask.reshape(x.shape[0], -1) # (N, S*E)
+        print('input_mask.shape ', input_mask.shape)
         
 
         # flip: reverse order in given axis: we want to
         # reverse time series order (last dimension) 
         residuals = x.flip(dims=(-1,)) # shape:  (N, S*E)
-        print('CP0 ', residuals.shape, input_mask.shape)
+        print('residuals.shape ', residuals.shape)
         
 
 
 
         for i, block in enumerate(self.blocks):
             backcast, block_forecast = block(residuals) # forecast: (N, E*T), backcast: (N,E*S)
-            print('CP1 ', backcast.shape, block_forecast.shape)
+            print('backcast and forecast shape ', backcast.shape, block_forecast.shape)
             #print(block_forecast)
             
             residuals = (residuals - backcast) * input_mask

models/nbeats/nbeats_network.py
@@ -474,7 +474,8 @@ class NBEATSTrainingNetwork(nn.Module):
         forecast = self.nb_model.forward(x_ts=time_series_inputs, x_tf=time_feat_inputs, x_s=static_inputs, pad_mask=pad_mask) #shape: (N, T, E)
 
 
-  
+        # this was added on 1.11.2021
+        forecast = forecast * scale
 
 
 
@@ -491,7 +492,11 @@ class NBEATSTrainingNetwork(nn.Module):
             loss = self.mape_loss(forecast, future_target)
         elif self.loss_function == "MASE":
             loss = self.mase_loss(
-                forecast, future_target, past_target / scale, self.periodicity
+                # forecast, future_target, past_target / scale, self.periodicity
+
+                # as of 1.11.2021 this was changed and past target is no longer scaled
+                # to be on the same scale as forecast and future_targets
+                forecast, future_target, past_target, self.periodicity
             )
         elif self.loss_function == "MSE":
             loss = torch.nn.MSELoss()(forecast, future_target)